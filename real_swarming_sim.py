304b8eff48945f5d70ac124d89c10d3e36dff621
nema <nemanja.antonic99@gmail.com>
2024-07-17 10:01:11 +0200

    added multithreading

--- real_swarming_sim.py
+++ real_swarming_sim.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from datetime import datetime
 import random
 
@@ -190,200 +191,203 @@ def save_parameters(directory, rho0):
 
 sigma, gamma, beta, alpha, D, beta_a, alpha_a, D_a, gamma_a, s_a, beta_r, alpha_r, D_r, gamma_r, s_r, scale, rho_max, cushion = updateParameters(PARAMETERS_FILE)
 
-def run(args=None):
-    for b_start in range(20, 256, 20):
-        global sigma, gamma, beta, alpha, D, beta_a, alpha_a, D_a, gamma_a, s_a, beta_r, alpha_r, D_r, gamma_r, s_r, dt
-        if args is None:
-            directory = "../my_swarming_results/distance/b_start_" + str(b_start)
-            #while os.path.isdir(directory):
-            #    directory = "../my_swarming_results/sim_" + str(int(directory.split("/")[-1].split("_")[-1]) + 1)
-        else:
-            gen = str(args[0])
-            i = str(args[1])
+def run(b_start, args=None):
+
+    global sigma, gamma, beta, alpha, D, beta_a, alpha_a, D_a, gamma_a, s_a, beta_r, alpha_r, D_r, gamma_r, s_r, dt
+    if args is None:
+        directory = "../my_swarming_results/distance/b_start_" + str(b_start)
+        #while os.path.isdir(directory):
+        #    directory = "../my_swarming_results/sim_" + str(int(directory.split("/")[-1].split("_")[-1]) + 1)
+    else:
+        gen = str(args[0])
+        i = str(args[1])
+        now = datetime.now()
+
+        # Format as string
+        now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+        # Use this string in your directory name
+        directory = f"my_swarming_results_optimisation/sim_{now_str}/gen_{gen}/ind_{i}"
+        while os.path.isdir(directory):
             now = datetime.now()
 
             # Format as string
             now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
-
-            # Use this string in your directory name
             directory = f"my_swarming_results_optimisation/sim_{now_str}/gen_{gen}/ind_{i}"
-            while os.path.isdir(directory):
-                now = datetime.now()
+    time_integration = "euler"
+    print("using: ", time_integration)
+    eps_min = 1e3
+    rho0 = 55417298.26084167
+    if RHO_0_FROM_FILE:
+        with open(PARAMETERS_FILE, 'r') as file:
+            lines = file.readlines()
+            rho0 = float(lines[-1].split("=")[1])
+    #rho0 = 55417298.26084167
+    step_max = STEP_MAX
+    logging = True
+    make_video = False
+    # Read parameters from text file
+    rho_matrices = []
+    if args is not None:
+        parameter_dir = args[2]
+        if not parameter_dir.endswith(".json"):
+            sigma, gamma, dc, k0, D, Dc, kc = updateParameters(
+                parameter_dir)
+    if NUMBER_OF_SPOTS == 1:
+        rho, U, Ua, Ur, t, step = initial_conditions(rho0, gamma_a, s_a, gamma_r, s_r)
+    else:
+        size_of_spawn = SIZE_OF_SPAWN
+        size_of_a = SIZE_OF_A
+        size_of_b = SIZE_OF_B
+        ca0 = rho0
+        cb0 = rho0
+        rho, U, Ua, Ur, t, step = initial_conditions_2_spots(rho0, size_of_spawn, size_of_a, size_of_b, ca0, cb0, gamma_a, s_a, gamma_r, s_r)
 
-                # Format as string
-                now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
-                directory = f"my_swarming_results_optimisation/sim_{now_str}/gen_{gen}/ind_{i}"
-        time_integration = "euler"
-        print("using: ", time_integration)
-        eps_min = 1e3
-        rho0 = 55417298.26084167
-        if RHO_0_FROM_FILE:
-            with open(PARAMETERS_FILE, 'r') as file:
-                lines = file.readlines()
-                rho0 = float(lines[-1].split("=")[1])
-        #rho0 = 55417298.26084167
-        step_max = STEP_MAX
-        logging = True
-        make_video = False
-        # Read parameters from text file
-        rho_matrices = []
-        if args is not None:
-            parameter_dir = args[2]
-            if not parameter_dir.endswith(".json"):
-                sigma, gamma, dc, k0, D, Dc, kc = updateParameters(
-                    parameter_dir)
-        if NUMBER_OF_SPOTS == 1:
-            rho, U, Ua, Ur, t, step = initial_conditions(rho0, gamma_a, s_a, gamma_r, s_r)
-        else:
-            size_of_spawn = SIZE_OF_SPAWN
-            size_of_a = SIZE_OF_A
-            size_of_b = SIZE_OF_B
-            ca0 = rho0
-            cb0 = rho0
-            rho, U, Ua, Ur, t, step = initial_conditions_2_spots(rho0, size_of_spawn, size_of_a, size_of_b, ca0, cb0, gamma_a, s_a, gamma_r, s_r)
+    rho, U, Ua, Ur, t, step = initial_conditions_from_center(rho0, gamma_a, s_a, gamma_r, s_r, b_start)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
 
-        rho, U, Ua, Ur, t, step = initial_conditions_from_center(rho0, gamma_a, s_a, gamma_r, s_r, b_start)
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
+    #save parameters in the directory
+    save_parameters(directory, rho0)
+    # Main loop
+    pbar = tqdm(total=step_max)
+    success = False
+    sigma_times_scale = sigma * scale
+    while step < step_max:
 
-        #save parameters in the directory
-        save_parameters(directory, rho0)
-        # Main loop
-        pbar = tqdm(total=step_max)
-        success = False
-        sigma_times_scale = sigma * scale
-        while step < step_max:
+        if time_integration == "euler":
 
-            if time_integration == "euler":
+            gradient_x_rho = gradientX(rho)
+            gradient_y_rho = gradientY(rho)
+            #gradient_x_U = gradientX(U)
+            #gradient_y_U = gradientY(U)
+            V = -beta * np.log(alpha + U)# - beta_a * np.log(alpha_a + Ua) - beta_r * np.log(alpha_r + Ur) + sigma_times_scale*(1+np.tanh((rho - rho_max)/cushion))
+            if ATTRACTANT:
+                V-= beta_a * np.log(alpha_a + Ua)
+                dUa = -gamma_a * Ua + D_a * laplacian(Ua) + s_a * rho
+                Ua = dUa * dt + Ua
+            if REPELLENT:
+                V-= beta_r * np.log(alpha_r + Ur)
+                dUr = -gamma_r * Ur + D_r * laplacian(Ur) + s_r * rho
+            if V_RHO:
+                V+= sigma_times_scale*(1+np.tanh((rho - rho_max)/cushion))
+                Ur = dUr * dt + Ur
+            drho = gradientX(rho * gradientX(V) + sigma * gradient_x_rho) + gradientY(
+                rho * gradientY(V) + sigma * gradient_y_rho)
 
-                gradient_x_rho = gradientX(rho)
-                gradient_y_rho = gradientY(rho)
-                #gradient_x_U = gradientX(U)
-                #gradient_y_U = gradientY(U)
-                V = -beta * np.log(alpha + U)# - beta_a * np.log(alpha_a + Ua) - beta_r * np.log(alpha_r + Ur) + sigma_times_scale*(1+np.tanh((rho - rho_max)/cushion))
-                if ATTRACTANT:
-                    V-= beta_a * np.log(alpha_a + Ua)
-                    dUa = -gamma_a * Ua + D_a * laplacian(Ua) + s_a * rho
-                    Ua = dUa * dt + Ua
-                if REPELLENT:
-                    V-= beta_r * np.log(alpha_r + Ur)
-                    dUr = -gamma_r * Ur + D_r * laplacian(Ur) + s_r * rho
-                if V_RHO:
-                    V+= sigma_times_scale*(1+np.tanh((rho - rho_max)/cushion))
-                    Ur = dUr * dt + Ur
-                drho = gradientX(rho * gradientX(V) + sigma * gradient_x_rho) + gradientY(
-                    rho * gradientY(V) + sigma * gradient_y_rho)
-
-                dU = -gamma * U + D * laplacian(U)
+            dU = -gamma * U + D * laplacian(U)
 
 
 
-                rho = (drho) * dt + rho
-                #add small noise to rho where rho>0:
-                #rho[rho>0] += rho[rho>0] * random.uniform(-0.001, 0.001)
-                U = dU * dt + U
+            rho = (drho) * dt + rho
+            #add small noise to rho where rho>0:
+            #rho[rho>0] += rho[rho>0] * random.uniform(-0.001, 0.001)
+            U = dU * dt + U
 
 
 
-                # check for non numerical values
-                if np.any(np.isnan(rho)) or np.any(np.isnan(U)):
-                    Exception("Non numerical values")
-                    break
+            # check for non numerical values
+            if np.any(np.isnan(rho)) or np.any(np.isnan(U)):
+                Exception("Non numerical values")
+                break
 
-                # check for infinities
-                if np.any(rho) > 10e16:
-                    Exception("Rho is too large")
-                    break
-
-            # add 10e7 (100/mm^2 = 10e7/mm^2) to Ua in the target area
-            #Ua[236:276, 236:276] += 10e9
-
-            # force Ua and Ur positive
-            U[U < 0] = 0
-            rho[rho < 0] = 0
-
-            # do not let Ua and Ur explode:
-            #U[U > 10e16] = 10e16
-
-
-            t = t + dt
-
-            eps = np.max(np.abs(drho))
-
-            if eps>10e16 or np.max(rho)>10e16:
+            # check for infinities
+            if np.any(rho) > 10e16:
                 Exception("Rho is too large")
                 break
-            '''
-            scaled_rho = 10e7 * rho
-            comparison_a = Ua > scaled_rho
-            comparison_r = Ur > scaled_rho
-    
-            if np.any(comparison_a) or np.any(comparison_r):
-                break
-            '''
 
-            if logging and step % 10000 == 0:
-                if make_video:
-                    rho_matrices.append(rho)
-                # save matrices every 10000 steps
-                np.save(directory + f"/rho_{step}.npy", rho)
-                np.save(directory + f"/U_{step}.npy", U)
-                if ATTRACTANT:
-                    np.save(directory + f"/Ua_{step}.npy", Ua)
-                if REPELLENT:
-                    np.save(directory + f"/Ur_{step}.npy", Ur)
+        # add 10e7 (100/mm^2 = 10e7/mm^2) to Ua in the target area
+        #Ua[236:276, 236:276] += 10e9
+
+        # force Ua and Ur positive
+        U[U < 0] = 0
+        rho[rho < 0] = 0
+
+        # do not let Ua and Ur explode:
+        #U[U > 10e16] = 10e16
 
 
+        t = t + dt
 
-            if eps < eps_min or step == step_max - 1:
+        eps = np.max(np.abs(drho))
+
+        if eps>10e16 or np.max(rho)>10e16:
+            Exception("Rho is too large")
+            break
+        '''
+        scaled_rho = 10e7 * rho
+        comparison_a = Ua > scaled_rho
+        comparison_r = Ur > scaled_rho
+
+        if np.any(comparison_a) or np.any(comparison_r):
+            break
+        '''
+
+        if logging and step % 10000 == 0:
+            if make_video:
                 rho_matrices.append(rho)
-                # save matrices every 10000 steps
-                np.save(directory + f"/rho_{step}.npy", rho)
-                np.save(directory + f"/U_{step}.npy", U)
-                if ATTRACTANT:
-                    np.save(directory + f"/Ua_{step}.npy", Ua)
-                if REPELLENT:
-                    np.save(directory + f"/Ur_{step}.npy", Ur)
-                print("saving in " + directory + f"/rho_{step}.npy")
-                success = True
-                break
-            pbar.update(1)
-            step += 1
-        print("eps: ", eps)
+            # save matrices every 10000 steps
+            np.save(directory + f"/rho_{step}.npy", rho)
+            np.save(directory + f"/U_{step}.npy", U)
+            if ATTRACTANT:
+                np.save(directory + f"/Ua_{step}.npy", Ua)
+            if REPELLENT:
+                np.save(directory + f"/Ur_{step}.npy", Ur)
 
-        if make_video:
-            fig, ax = plt.subplots()
-            if len(rho_matrices) == 0:
-                rho_matrices.append(rho)
-            cax = ax.imshow(rho_matrices[0], cmap='viridis', interpolation='nearest')
 
-            def update(frame):
-                cax.set_array(rho_matrices[frame])
-                return [cax]
 
-            ani = animation.FuncAnimation(fig, update, frames=len(rho_matrices), blit=True)
-            ani.save('rho_evolution_oxy_40.mp4', writer='ffmpeg', fps=1, dpi=100)
+        if eps < eps_min or step == step_max - 1:
+            rho_matrices.append(rho)
+            # save matrices every 10000 steps
+            np.save(directory + f"/rho_{step}.npy", rho)
+            np.save(directory + f"/U_{step}.npy", U)
+            if ATTRACTANT:
+                np.save(directory + f"/Ua_{step}.npy", Ua)
+            if REPELLENT:
+                np.save(directory + f"/Ur_{step}.npy", Ur)
+            print("saving in " + directory + f"/rho_{step}.npy")
+            success = True
+            break
+        pbar.update(1)
+        step += 1
+    print("eps: ", eps)
 
-        #show(rho, directory)
-        #save in directory the pdf of the final density
-        save_final_plot(rho, directory)
+    if make_video:
+        fig, ax = plt.subplots()
+        if len(rho_matrices) == 0:
+            rho_matrices.append(rho)
+        cax = ax.imshow(rho_matrices[0], cmap='viridis', interpolation='nearest')
 
-        if success:
-            # from clustering2 import evaluate
-            # c, kurt, bins, ys = evaluate(directory + f"/rho_{step}.npy", 50)
-            c = get_clustering_metric(rho)
-            print(c)
-        else:
-            c = -1
-            t = step_max * dt
+        def update(frame):
+            cax.set_array(rho_matrices[frame])
+            return [cax]
+
+        ani = animation.FuncAnimation(fig, update, frames=len(rho_matrices), blit=True)
+        ani.save('rho_evolution_oxy_40.mp4', writer='ffmpeg', fps=1, dpi=100)
+
+    #show(rho, directory)
+    #save in directory the pdf of the final density
+    save_final_plot(rho, directory)
+
+    if success:
+        # from clustering2 import evaluate
+        # c, kurt, bins, ys = evaluate(directory + f"/rho_{step}.npy", 50)
+        c = get_clustering_metric(rho)
+        print(c)
+    else:
+        c = -1
+        t = step_max * dt
 
     return c, t
 
 if __name__ == "__main__":
     arg = sys.argv[1]
     if arg=="run":
-        #instaead of running only 1 instance, run an instance
-        run()
+
+        params = list(range(20, 256, 20))
+        with multiprocessing.Pool() as pool:
+            results = pool.map(run, params)
+        #run()
     #'''
     else:
         from clustering2 import evaluate, create_matrix_from_tsv
